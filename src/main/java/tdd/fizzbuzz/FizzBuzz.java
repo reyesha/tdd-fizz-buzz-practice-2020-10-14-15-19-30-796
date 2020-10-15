18357afdf40deda5d63b345782eae76a03b0d906
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-15 14:34:01 +0800

    [FizzBuzz] [Hannah] feature: added logic for order number multiple of 3 & 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public String countOff(int orderNumber) {
 
-        if (orderNumber % 3 == 0){
+        if (((orderNumber % 3) == 0) && ((orderNumber% 5) == 0)){
+            return "FizzBuzz";
+        }else if (orderNumber % 3 == 0){
             return "Fizz";
         }else if (orderNumber % 5 == 0) {
             return "Buzz";
