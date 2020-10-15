c1f22af8c95da840c0c4e19129daafb6c38c05a8
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-15 13:57:32 +0800

    [FizzBuzz] [Hannah] test: added test for given order 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_given_order_number_1() {
+
+        int orderNumber = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.countOff(orderNumber);
+
+        assertEquals("1", actual);
+    }
 }
